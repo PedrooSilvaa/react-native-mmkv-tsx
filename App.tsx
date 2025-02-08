5c40e93a245311202e35eaba8966a24b9243fe98
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2025-02-08 11:38:20 -0300

    feat: studying about MMKV as a local storage solution in React Native

--- App.tsx
+++ App.tsx
@@ -1,118 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useState } from 'react';
+import { Button, ScrollView, Text, TextInput, View } from 'react-native';
+import { styles } from './styles';
+import { MMKV } from 'react-native-mmkv';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const storage = new MMKV({id: 'myapp'});
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type User = {
+  name: string;
+  email: string;
+}
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App(){
+  
+  const [dataUser, setDataUser] = useState<User>({ name: "", email: "" });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  
+  function handleSave() {
+      const data = storage.getString('user');
+      
+      if (data) {
+        const parseData = JSON.parse(data);
+        setDataUser(parseData); 
+      }
+
+      setDataUser((prevDataUser) => {
+        if (!prevDataUser) return { name, email };
+          const updatedUser = {
+              ...prevDataUser,
+              name: prevDataUser.name ? prevDataUser.name + ";" + name + ";" + email + "/n" : name + ";" + email + "/n" ,
+              email: prevDataUser.email ? prevDataUser.email + ";" + email + "/n"  : email + "/n" 
+              // name : "",
+              // email: ""
+          };
+
+          storage.set('user', JSON.stringify(updatedUser));
+
+          return updatedUser;
+      });
+  }
+
+
+  useEffect(() => {
+    fetchUser();
+  }, [dataUser]);
+
+  const [user, setUser] = useState<User>()
+
+  function fetchUser(){
+    const data = storage.getString('user');
+    setUser(data? JSON.parse(data) : {})
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+    <View style={styles.container}>
+      
+      <TextInput 
+        placeholder='Nome' 
+        style={styles.input}
+        onChangeText={setName}
+        />   
+
+      <TextInput 
+        placeholder='Email' 
+        style={styles.input}
+        onChangeText={setEmail}
+        />  
+
+      <Button 
+      title='Salvar' 
+      onPress={() => handleSave()}
+      /> 
+
+      <Text style={styles.text}>
+        {user?.name} - {user?.email}
       </Text>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
